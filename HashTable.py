a737c32cdada4a8505a9a4462fa5607f971e5ce1
PolleAnker <thementoryay@gmail.com>
2021-04-18 14:13:05 +0200

    Lil update, new main file for testing

--- HashTable.py
+++ HashTable.py
@@ -2,12 +2,15 @@ class HashTable:
     def __init__(self):
         self.MAX = 100
         self.arr = [None for i in range(self.MAX)]
+        print('Created')
 
     def get_hash(self, key):
         h = 0
         for char in key:
-            h += ord(key)
+            h += ord(char)
+        print(h % self.MAX)
         return h % self.MAX
 
-
-    
+    def add(self, key, val):
+        h = self.get_hash(key)
+        self.arr[h] = val
---
+++ main.py
@@ -0,0 +1,6 @@
+from HashTable import HashTable
+
+t = HashTable()
+
+t.add('march 6', 130)
+t.get_hash('march 6')
