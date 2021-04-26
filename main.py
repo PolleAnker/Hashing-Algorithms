a088c3f3ce0e10634a663a06fe71c2c07254b9f3
PolleAnker <thementoryay@gmail.com>
2021-04-26 13:23:41 +0200

    extra text for print in main

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from OpenAddressing import OpenAddressing
 from Chaining import Chaining
 
 
-
 # Set up hash maps to be filled with data (with sizes corresponding to size of data set)
 OpenHash_150 = OpenAddressing(150, "Division", "Linear")
 OpenHash_500 = OpenAddressing(500, "Division", "Linear")
@@ -13,6 +12,7 @@ Chaining_500 = Chaining(500, "Division")
 Chaining_3000 = Chaining(3000, "Division")
 
 
+# Open file and hash the contents to the selected hashmap
 with open("dataset(150).txt", "r") as f:
     for line in f:
         tokens = line.split(',')
@@ -23,3 +23,9 @@ with open("dataset(150).txt", "r") as f:
             Chaining_150[_id] = name
         except:
             print("Invalid entry, ignoring row")
+
+print("OPEN ADDRESSING METHOD")
+print(OpenHash_150.arr)
+print("__________________________________________________________________________")
+print("CHAINING METHOD")
+print(Chaining_150.arr)
