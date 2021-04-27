dc3be6057f0a4831327a0bda2de05a6c11e4abb7
PolleAnker <thementoryay@gmail.com>
2021-04-27 14:07:45 +0200

    Evaluation function for each of the datasets
    All function calls made in the main.py file, however, all but the 150 sized function calls are commented out, for ease of use

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import HashFunctions
 import random
 
 # Set up hash maps to be filled with data (with sizes corresponding to size of data set)
-OpenHash = OpenAddressing(3000, "Division", "Linear")
-ChainHash = Chaining(3000, "Division")
+OpenHash = OpenAddressing(150, "Division", "Linear")
+ChainHash = Chaining(150, "Division")
 
 # Randomize which value to look for, for deletion and searching (can be non-existent in dataset)
 item_to_delete = random.randrange(0,3000)
@@ -18,6 +18,10 @@ item_to_find = random.randrange(0,3000)
 print("Item to find is " + str(item_to_find) + " and item to delete is " + str(item_to_delete))
 
 print("OPEN HASHING")
-HashFunctions.hash_evaluation(OpenHash, "dataset(3000).txt", item_to_delete, item_to_find)
+HashFunctions.hash_evaluation(OpenHash, "dataset(150).txt", item_to_delete, item_to_find)
+#HashFunctions.hash_evaluation(OpenHash, "dataset(500).txt", item_to_delete, item_to_find)
+#HashFunctions.hash_evaluation(OpenHash, "dataset(3000).txt", item_to_delete, item_to_find)
 print("CHAIN HASHING")
-HashFunctions.hash_evaluation(ChainHash, "dataset(3000).txt", item_to_delete, item_to_find)
+HashFunctions.hash_evaluation(ChainHash, "dataset(150).txt", item_to_delete, item_to_find)
+#HashFunctions.hash_evaluation(ChainHash, "dataset(500).txt", item_to_delete, item_to_find)
+#HashFunctions.hash_evaluation(ChainHash, "dataset(3000).txt", item_to_delete, item_to_find)
